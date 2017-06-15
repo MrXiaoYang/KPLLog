e507aa453a5cef4ebe57694cf4449808665c25a9
MrXiaoYang <582385082@qq.com>
2017-06-15 14:15:06 +0800

    0.1.0

---
+++ Example/Pods/Target Support Files/KPLLog/KPLLog-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_KPLLog : NSObject
+@end
+@implementation PodsDummy_KPLLog
+@end
---
+++ Example/Pods/Target Support Files/Pods-KPLLog_Tests/Pods-KPLLog_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KPLLog_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_KPLLog_Tests
+@end
