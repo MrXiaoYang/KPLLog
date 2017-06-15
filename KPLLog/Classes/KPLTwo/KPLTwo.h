83a83bbb55cc413021348a23f93fec2588c0fee0
MrXiaoYang <582385082@qq.com>
2017-06-15 14:29:45 +0800

    x

---
+++ KPLLog/Classes/KPLTwo/KPLTwo.h
@@ -0,0 +1,13 @@
+//
+//  KPLTwo.h
+//  Pods
+//
+//  Created by apple on 17/6/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KPLTwo : NSObject
++ (void)kpl_two;
+@end
---
+++ KPLLog/Classes/KPLTwo/KPLTwo.m
@@ -0,0 +1,15 @@
+//
+//  KPLTwo.m
+//  Pods
+//
+//  Created by apple on 17/6/15.
+//
+//
+
+#import "KPLTwo.h"
+
+@implementation KPLTwo
++ (void)kpl_two {
+    NSLog(@"two");
+}
+@end
